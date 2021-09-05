ce4efe81365be61620d4332351603ab35efc977e
Louis DESPLANCHE <louis.desplanche@edu.esiee.fr>
2021-09-05 12:43:13 +0200

    Update dependencies

--- config/database.js
+++ config/database.js
@@ -1,11 +1,17 @@
-module.exports = ({ env }) => ({
+module.exports = ({env}) => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
+        client: env('DATABASE_CLIENT', 'sqlite'),
         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        host: env('DATABASE_HOST', ''),
+        port: env.int('DATABASE_PORT', 0),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', ''),
+        ssl: env('DATABASE_SSL', false),
       },
       options: {
         useNullAsDefault: true,
